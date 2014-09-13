8225e1cff940a10344453b09d939677d4b3632ac
Alex Kang <akang95@gmail.com>
2014-09-13 00:39:04 -0700

    Switched to ogg files, still working out some browser compatibility issues

--- montage/scripts/audio.js
+++ montage/scripts/audio.js
@@ -1,12 +1,12 @@
-var interventionFire = new Audio("sounds/intervention_fire.mp3");
-var smokeWeed = new Audio("sounds/smoke_weed.mp3");
+var interventionFire = new Audio("sounds/intervention_fire.ogg");
+var smokeWeed = new Audio("sounds/smoke_weed.ogg");
 var hitmarker;
 var userAgent = navigator.userAgent.toLowerCase();
 
 if (userAgent.indexOf('firefox') < 0) {
-     hitmarker = new Audio("sounds/hitmarker.mp3");
+     hitmarker = new Audio("sounds/hitmarker.ogg");
 } else {
-	hitmarker = new Audio("sounds/hitmarker_alt.mp3");
+	hitmarker = new Audio("sounds/hitmarker_alt.ogg");
 }
 
 if (userAgent.indexOf('chrome') <= -1 && userAgent.indexOf('safari') > -1) {
@@ -15,34 +15,34 @@ if (userAgent.indexOf('chrome') <= -1 && userAgent.indexOf('safari') > -1) {
 
 var hornSounds = 
 	[
-	new Audio("sounds/horn/horn_c.mp3"),
-	new Audio("sounds/horn/horn_c_sharp.mp3"),
-	new Audio("sounds/horn/horn_d.mp3"),
-	new Audio("sounds/horn/horn_d_sharp.mp3"),
-	new Audio("sounds/horn/horn_e.mp3"),
-	new Audio("sounds/horn/horn_f.mp3"),
-	new Audio("sounds/horn/horn_f_sharp.mp3"),
-	new Audio("sounds/horn/horn_g.mp3"),
-	new Audio("sounds/horn/horn_g_sharp.mp3"),
-	new Audio("sounds/horn/horn_a.mp3"),
-	new Audio("sounds/horn/horn_a_sharp.mp3"),
-	new Audio("sounds/horn/horn_b.mp3"),
-	new Audio("sounds/horn/horn_c_high.mp3")
+	new Audio("sounds/horn/horn_c.ogg"),
+	new Audio("sounds/horn/horn_c_sharp.ogg"),
+	new Audio("sounds/horn/horn_d.ogg"),
+	new Audio("sounds/horn/horn_d_sharp.ogg"),
+	new Audio("sounds/horn/horn_e.ogg"),
+	new Audio("sounds/horn/horn_f.ogg"),
+	new Audio("sounds/horn/horn_f_sharp.ogg"),
+	new Audio("sounds/horn/horn_g.ogg"),
+	new Audio("sounds/horn/horn_g_sharp.ogg"),
+	new Audio("sounds/horn/horn_a.ogg"),
+	new Audio("sounds/horn/horn_a_sharp.ogg"),
+	new Audio("sounds/horn/horn_b.ogg"),
+	new Audio("sounds/horn/horn_c_high.ogg")
 	];
 
 var weedSounds = 
 	[
-	new Audio("sounds/weed/weed_c.mp3"),
-	new Audio("sounds/weed/weed_c_sharp.mp3"),
-	new Audio("sounds/weed/weed_d.mp3"),
-	new Audio("sounds/weed/weed_d_sharp.mp3"),
-	new Audio("sounds/weed/weed_e.mp3"),
-	new Audio("sounds/weed/weed_f.mp3"),
-	new Audio("sounds/weed/weed_f_sharp.mp3"),
-	new Audio("sounds/weed/weed_g.mp3"),
-	new Audio("sounds/weed/weed_g_sharp.mp3"),
-	new Audio("sounds/weed/weed_a.mp3"),
-	new Audio("sounds/weed/weed_a_sharp.mp3"),
-	new Audio("sounds/weed/weed_b.mp3"),
-	new Audio("sounds/weed/weed_c_high.mp3")
+	new Audio("sounds/weed/weed_c.ogg"),
+	new Audio("sounds/weed/weed_c_sharp.ogg"),
+	new Audio("sounds/weed/weed_d.ogg"),
+	new Audio("sounds/weed/weed_d_sharp.ogg"),
+	new Audio("sounds/weed/weed_e.ogg"),
+	new Audio("sounds/weed/weed_f.ogg"),
+	new Audio("sounds/weed/weed_f_sharp.ogg"),
+	new Audio("sounds/weed/weed_g.ogg"),
+	new Audio("sounds/weed/weed_g_sharp.ogg"),
+	new Audio("sounds/weed/weed_a.ogg"),
+	new Audio("sounds/weed/weed_a_sharp.ogg"),
+	new Audio("sounds/weed/weed_b.ogg"),
+	new Audio("sounds/weed/weed_c_high.ogg")
 	];
