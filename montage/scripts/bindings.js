9bd60029ce2fd36132c57088e7c8e769ce24564a
Alex Kang <akang95@gmail.com>
2014-09-17 18:39:10 -0700

    Removed global variables and tried to improve performance across Chrome and Firefox

--- montage/scripts/bindings.js
+++ montage/scripts/bindings.js
@@ -2,73 +2,73 @@ Mousetrap.bind('`', function() {
 	var weedChecked = $('#weedCheck').prop('checked');
 	$('#weedCheck').prop('checked', !weedChecked);
 
-	switchMode();
+	MontageBoard.switchMode();
 });
 
 Mousetrap.bind('ctrl', function() {
-	playSound('intervention');
+	MontageBoard.playSound('intervention');
 });
 
 Mousetrap.bind('command', function() {
-	playSound('intervention');
+	MontageBoard.playSound('intervention');
 });
 
 Mousetrap.bind('shift', function() {
-	playSound('hitmarker');
+	MontageBoard.playSound('hitmarker');
 });
 
 Mousetrap.bind('space', function() {
-	playSound('smokeWeed');
+	MontageBoard.playSound('smokeWeed');
 });
 
 Mousetrap.bind('s', function() {
-	playNote(0, 's');
+	MontageBoard.playNote(0, 's');
 });
 
 Mousetrap.bind('e', function() {
-	playNote(1, 'e');
+	MontageBoard.playNote(1, 'e');
 });
 
 Mousetrap.bind('d', function() {
-	playNote(2, 'd');
+	MontageBoard.playNote(2, 'd');
 });
 
 Mousetrap.bind('r', function() {
-	playNote(3, 'r');
+	MontageBoard.playNote(3, 'r');
 });
 
 Mousetrap.bind('f', function() {
-	playNote(4, 'f');
+	MontageBoard.playNote(4, 'f');
 });
 
 Mousetrap.bind('g', function() {
-	playNote(5, 'g');
+	MontageBoard.playNote(5, 'g');
 });
 
 Mousetrap.bind('y', function() {
-	playNote(6, 'y');
+	MontageBoard.playNote(6, 'y');
 });
 
 Mousetrap.bind('h', function() {
-	playNote(7, 'h');
+	MontageBoard.playNote(7, 'h');
 });
 
 Mousetrap.bind('u', function() {
-	playNote(8, 'u');
+	MontageBoard.playNote(8, 'u');
 });
 
 Mousetrap.bind('j', function() {
-	playNote(9, 'j');
+	MontageBoard.playNote(9, 'j');
 });
 
 Mousetrap.bind('i', function() {
-	playNote(10, 'i');
+	MontageBoard.playNote(10, 'i');
 });
 
 Mousetrap.bind('k', function() {
-	playNote(11, 'k');
+	MontageBoard.playNote(11, 'k');
 });
 
 Mousetrap.bind('l', function() {
-	playNote(12, 'l');
+	MontageBoard.playNote(12, 'l');
 });
