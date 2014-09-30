6070fbae73388ad1127f0d5bf4aec2166b044351
Alex Kang <akang95@gmail.com>
2014-09-29 23:02:45 -0700

    Added personal project page

---
+++ projects/scripts/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$('.list_element').mousedown(function() {
+		$(this).addClass("list_element_pressed");
+	});
+
+	$('.list_element').mouseup(function() {
+		$(this).removeClass("list_element_pressed");
+	});
+
+	$('#loopboard').click(function() {
+		window.location.href = "https://play.google.com/store/apps/details?id=com.alexkang.loopboard";
+	});
+
+	$('#matrix').click(function() {
+		window.location.href = "https://play.google.com/store/apps/details?id=com.alexkang.x3matrixcalculator";
+	});
+
+	$('#bluechat').click(function() {
+		window.location.href = "https://github.com/AlexKang/blue-chat";
+	});
+
+	$('#glamour').click(function() {
+		window.location.href = "http://alexkang.github.io/glamour-beauty-supply/index.html";
+	});
+
+	$('#montageboard').click(function() {
+		window.location.href = "http://alexkang.github.io/montage/";
+	});
+
+	$('#swagcribz').click(function() {
+		window.location.href = "http://swagcribz.azurewebsites.net/";
+	});
+});
