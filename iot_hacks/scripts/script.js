9edf0a54de880e01753216715e9738de35fa8dc2
Alex Kang <akang95@gmail.com>
2016-09-01 02:04:50 -0700

    Added IoT Hacks website

---
+++ iot_hacks/scripts/script.js
@@ -0,0 +1,49 @@
+(function() {
+	var WRITE_DELAY = 250;
+	var DELETE_DELAY = 100;
+	var THING_DURATION = 1000;
+	var BLANK_DURATION = 500;
+	var PREFIX = "internet of ";
+	var THINGS = ["things", "toasters", "light bulbs", "rice cookers", "coffee makers", "vapes",
+				  "toothbrushes", "printers", "vacuum cleaners", "speakers", "cameras", "doors"];
+
+	function writeThing(thing, currIndex, onCompletion) {
+		if (currIndex > thing.length) {
+			onCompletion();
+			return;
+		}
+		$(".header-text").html(PREFIX + thing.substring(0, currIndex));
+		setTimeout(function() { writeThing(thing, currIndex + 1, onCompletion) }, WRITE_DELAY);
+	}
+
+	function deleteThing(thing, currIndex, onCompletion) {
+		if (currIndex == -1) {
+			onCompletion();
+			return;
+		}
+		$(".header-text").html(PREFIX + thing.substring(0, currIndex));
+		setTimeout(function() { deleteThing(thing, currIndex - 1, onCompletion) }, DELETE_DELAY);
+	}
+
+	function onThingCompleted(thingIndex) {
+		return function() {
+			setTimeout(function() {
+				var thing = THINGS[thingIndex];
+				deleteThing(thing, thing.length, onThingDeleted(thingIndex))
+			}, THING_DURATION);
+		}
+	}
+
+	function onThingDeleted(thingIndex) {
+		return function() {
+			setTimeout(function() {
+				var nextIndex = (thingIndex + 1) % THINGS.length;
+				writeThing(THINGS[nextIndex], 0, onThingCompleted(nextIndex));
+			}, BLANK_DURATION);
+		}
+	}
+
+	$(document).ready(function() {
+		writeThing(THINGS[0], 0, onThingCompleted(0));
+	});
+})();
